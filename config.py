6b1bed5734ea6e31307ee1e5b19cfd7be7dcccf0
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-02-20 00:15:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/subhichiku/l2s",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "love")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
